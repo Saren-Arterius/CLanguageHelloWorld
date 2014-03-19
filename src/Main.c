c806b67aa1e861371a46bdff14a84e971434fc3f
Saren <ykmran@gmail.com>
2014-03-20 00:33:22 +0800

    Useless

--- src/Main.c
+++ src/Main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
-int main(void) {
+int* fibonnacci(int limit) {
+    int fib[100] = { 0 };
+    fib[0] = 0;
+    fib[1] = 1;
+    for (int i = 2; i < limit; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+    return fib;
+}
 
+int main(void) {
+    int **a = fibonnacci(10);
+    printf("%d", a[1]);
     return 0;
 }
+
+
